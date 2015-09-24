59afce4454eab05dab071580f5252e189f74b229
Alexey Chernysh <alexey.chernysh@gmail.com>
2015-09-24 14:25:39 +0700

    Shaterd preferences for last table key used

--- app/src/main/java/ru/chernysh/plasmatorchsetup/MainActivity.java
+++ app/src/main/java/ru/chernysh/plasmatorchsetup/MainActivity.java
@@ -1,20 +1,11 @@
 package ru.chernysh.plasmatorchsetup;
 
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.database.sqlite.SQLiteException;
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private static final String LOG_TAG = MainActivity.class.getName()+": ";
 
@@ -32,34 +23,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AutoCompleteTextView textView = (AutoCompleteTextView)findViewById(R.id.BrandName);
-        ArrayAdapter<String> brandAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, BRANDS);
-        textView.setAdapter(brandAdapter);
+        Spinner brandSpinner = (Spinner)findViewById(R.id.BrandName);
+        ArrayAdapter<String> brandAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, BRANDS);
+        brandAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        brandSpinner.setAdapter(brandAdapter);
 
-        Spinner spinner = (Spinner) findViewById(R.id.SeriesName);
-        ArrayAdapter<String> seriesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, SERIES);
+        Spinner seriesSpinner = (Spinner) findViewById(R.id.SeriesName);
+        ArrayAdapter<String> seriesAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, SERIES);
         seriesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(seriesAdapter);    }
+        seriesSpinner.setAdapter(seriesAdapter);    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
