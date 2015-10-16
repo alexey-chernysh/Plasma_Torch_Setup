3f02ecb9b8887a1525456818a7338c1a14a8f5e9
Alexey Chernysh <alexey.chernysh@gmail.com>
2015-10-16 17:51:16 +0700

    db dev advanced

--- app/src/main/java/ru/chernysh/plasmatorchsetup/MainActivity.java
+++ app/src/main/java/ru/chernysh/plasmatorchsetup/MainActivity.java
@@ -3,6 +3,11 @@ package ru.chernysh.plasmatorchsetup;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.ParseException;
 
 public class MainActivity extends Activity {
 
@@ -22,7 +27,7 @@ public class MainActivity extends Activity {
 
         Log.d(LOG_TAG, "OnCreate");
         initSpinners();
-
+        initThicknessEdit();
     }
 
     private void initSpinners(){
@@ -59,8 +64,8 @@ public class MainActivity extends Activity {
                 0,
                 series);
 
-        series.updateList(series_selected,brand_selected);
-        model.updateList(model_selected,series_selected);
+        series.updateList(series_selected, brand_selected);
+        model.updateList(model_selected, series_selected);
 
         final String material_table_name = getString(R.string.material_table);
         material = new TableWithSpinner(this.findViewById(android.R.id.content),
@@ -70,6 +75,32 @@ public class MainActivity extends Activity {
                 null,
                 0,
                 null);
+    }
+
+    private void initThicknessEdit(){
+        final EditText materialThicknessEdit = (EditText)findViewById(R.id.materialThickness);
+        int thicknessMul100 = (new StoredKey(App.getResourceString(R.string.preference_)
+                                                    +App.getResourceString(R.string.material_thickness) )).get();
+        Double thickness = ((double)thicknessMul100)/100.0;
+        materialThicknessEdit.setText(thickness.toString());
+        materialThicknessEdit.setOnKeyListener(new View.OnKeyListener(){
+            public boolean onKey(View v, int keyCode, KeyEvent event){
+                if(event.getAction() == KeyEvent.ACTION_DOWN &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // сохраняем текст, введенный до нажатия Enter в переменную
+                    String str = materialThicknessEdit.getText().toString();
+                    try {
+                        double thickness = Double.parseDouble(str);
+                        int thicknessMul100 = (int)(thickness*100.0);
+                        (new StoredKey(App.getResourceString(R.string.preference_)
+                                     + App.getResourceString(R.string.material_thickness) )).set(thicknessMul100);
+                    } catch (NumberFormatException nfe) {
+                    };
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
 }
