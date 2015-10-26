3de63971fb775d3aea9ead30dfed32546e9c1883
Alexey Chernysh <alexey.chernysh@gmail.com>
2015-10-26 16:55:49 +0700

    spinner bug fixed

--- app/src/main/java/ru/chernysh/plasmatorchsetup/MainActivity.java
+++ app/src/main/java/ru/chernysh/plasmatorchsetup/MainActivity.java
@@ -2,6 +2,7 @@ package ru.chernysh.plasmatorchsetup;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -9,14 +10,13 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     private static final String LOG_TAG = MainActivity.class.getName()+": ";
-    private int model_selected;
-    private TableWithSpinner model;
-    private TableWithSpinner series;
-    private TableWithSpinner brand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +26,15 @@ public class MainActivity extends Activity {
         Log.d(LOG_TAG, "OnCreate");
         initSpinners();
         initThicknessEdit();
+        initTable();
 
-        model.setSelected(model_selected);
-        int series_selected = model.getFilterKey();
-        series.setSelected(series_selected);
-        int brand_selected = series.getFilterKey();
-
-        brand.setSelected(brand_selected);
-        series.setSelected(series_selected);
-        model.setSelected(model_selected);
     }
 
     private void initSpinners(){
+        int model_selected;
+        TableWithSpinner model;
+        TableWithSpinner series;
+        TableWithSpinner brand;
 
         final String model_table_name = getString(R.string.model_table);
         final String series_table_name = getString(R.string.series_table);
@@ -63,6 +60,14 @@ public class MainActivity extends Activity {
         series.setUpperLevelSpinner(brand);
         brand.setLowerLevelSpinner(series);
 
+        model.setSelected(model_selected);
+        int series_selected = model.getFilterKey();
+        series.setSelected(series_selected);
+        int brand_selected = series.getFilterKey();
+
+        brand.setSelected(brand_selected);
+        series.setSelected(series_selected);
+        model.setSelected(model_selected);
     }
 
     private void initThicknessEdit(){
@@ -80,17 +85,17 @@ public class MainActivity extends Activity {
         Double thickness = ((double)thickness_х_100)/100.0;
         String thicknessString = thickness.toString();
         materialThicknessEdit.setText(thicknessString);
-        materialThicknessEdit.setOnKeyListener(new View.OnKeyListener(){
-            public boolean onKey(View v, int keyCode, KeyEvent event){
-                if(event.getAction() == KeyEvent.ACTION_DOWN &&
+        materialThicknessEdit.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN &&
                         (keyCode == KeyEvent.KEYCODE_ENTER)) {
                     // сохраняем текст, введенный до нажатия Enter в переменную
                     String str = materialThicknessEdit.getText().toString();
                     try {
                         double thickness = Double.parseDouble(str);
-                        int thickness_x_100 = (int)(thickness*100.0);
+                        int thickness_x_100 = (int) (thickness * 100.0);
                         (new StoredKey(App.getResourceString(R.string.preference_)
-                                     + App.getResourceString(R.string.material_thickness) )).set(thickness_x_100);
+                                + App.getResourceString(R.string.material_thickness))).set(thickness_x_100);
                     } catch (NumberFormatException nfe) {
                         // nothing to do
                         // waiting for suitable number
@@ -100,6 +105,26 @@ public class MainActivity extends Activity {
                 return false;
             }
         });
+    }
+
+    private void initTable() {
+        TableLayout table = (TableLayout)findViewById(R.id.plasmaSettingTable);
+        int nOfRow = 10;
+        for( int i=1; i<nOfRow; i++){
+            TableRow row= new TableRow(this);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+            row.setLayoutParams(lp);
+            int nOfColumns = 10;
+            for( int j = 0; j< nOfColumns; j++){
+                TextView tv = new TextView(this);
+                if((j%2)==0)
+                    tv.setText(Integer.toString(i*1000+j*10));
+                row.addView(tv);
+            }
+            if((i%2)>0) row.setBackgroundColor(Color.parseColor("#FFFFFFAA"));
+            else row.setBackgroundColor(Color.parseColor("#FFFFFFDD"));
+            table.addView(row,i);
+        }
     }
 
 }
