10e4d23873d3d14da9121d35b6cab917e2ca663a
Alexey Chernysh <alexey.chernysh@gmail.com>
2015-11-30 17:37:07 +0700

    3456

--- app/src/main/java/ru/chernysh/plasmatorchsetup/CuttingChart.java
+++ app/src/main/java/ru/chernysh/plasmatorchsetup/CuttingChart.java
@@ -5,11 +5,24 @@
 
 package ru.chernysh.plasmatorchsetup;
 
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
-/**
- * Created by Sales on 30.11.2015.
- */
 public class CuttingChart {
-    ArrayList<CuttingChartRecord> records;
+
+    ArrayList<CuttingChartColumn> columns;
+
+    public CuttingChart(){
+        columns = new ArrayList<CuttingChartColumn>(9);
+        columns.set(0, new CuttingChartColumn(R.string.process_header));
+        columns.set(1, new CuttingChartColumn(R.string.current_header));
+        columns.set(2, new CuttingChartColumn(R.string.thickness_header));
+        columns.set(3, new CuttingChartColumn(R.string.purpose_header));
+        columns.set(4, new CuttingChartColumn(R.string.arc_voltage_header));
+        columns.set(5, new CuttingChartColumn(R.string.arc_height_header));
+        columns.set(6, new CuttingChartColumn(R.string.pierce_height_header));
+        columns.set(7, new CuttingChartColumn(R.string.pierce_time_header));
+        columns.set(8, new CuttingChartColumn(R.string.kerf_offset_header));
+    }
 }
--- app/src/main/java/ru/chernysh/plasmatorchsetup/CuttingChartColumn.java
+++ app/src/main/java/ru/chernysh/plasmatorchsetup/CuttingChartColumn.java
@@ -5,11 +5,32 @@
 
 package ru.chernysh.plasmatorchsetup;
 
+import java.util.ArrayList;
+
 public class CuttingChartColumn {
 
     private final String header;
+    private final String dbAccessName;
+    ArrayList<Double> data;
 
-    public CuttingChartColumn(int headerStringId){
+    public CuttingChartColumn(int headerStringId, int columnHeaderId){
         header = App.getResourceString(headerStringId);
+        dbAccessName = App.getResourceString(columnHeaderId);
+    }
+
+    public void setData(int recordNum, double value){
+        data.add(value);
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public double getData(int recordNum){
+        return data.get(recordNum);
+    }
+
+    public String getDbAccessName() {
+        return dbAccessName;
     }
 }
