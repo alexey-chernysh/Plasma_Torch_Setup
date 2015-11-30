acefc70142e4e2abdfc7ef1a3c48b23a4933cd6f
Alexey Chernysh <alexey.chernysh@gmail.com>
2015-11-30 23:40:29 +0700

    fill settings table

--- app/src/main/java/ru/chernysh/plasmatorchsetup/CuttingChart.java
+++ app/src/main/java/ru/chernysh/plasmatorchsetup/CuttingChart.java
@@ -5,8 +5,6 @@
 
 package ru.chernysh.plasmatorchsetup;
 
-import android.widget.TextView;
-
 import java.util.ArrayList;
 
 public class CuttingChart {
@@ -15,14 +13,14 @@ public class CuttingChart {
 
     public CuttingChart(){
         columns = new ArrayList<CuttingChartColumn>(9);
-        columns.set(0, new CuttingChartColumn(R.string.process_header));
-        columns.set(1, new CuttingChartColumn(R.string.current_header));
-        columns.set(2, new CuttingChartColumn(R.string.thickness_header));
-        columns.set(3, new CuttingChartColumn(R.string.purpose_header));
-        columns.set(4, new CuttingChartColumn(R.string.arc_voltage_header));
-        columns.set(5, new CuttingChartColumn(R.string.arc_height_header));
-        columns.set(6, new CuttingChartColumn(R.string.pierce_height_header));
-        columns.set(7, new CuttingChartColumn(R.string.pierce_time_header));
-        columns.set(8, new CuttingChartColumn(R.string.kerf_offset_header));
+        columns.set(0, new CuttingChartColumn(R.string.process_header, R.string.process_table));
+        columns.set(1, new CuttingChartColumn(R.string.current_header, R.string.current_column_name));
+        columns.set(2, new CuttingChartColumn(R.string.thickness_header, R.string.thickness_column_name));
+        columns.set(3, new CuttingChartColumn(R.string.purpose_header, R.string.purpose_table));
+        columns.set(4, new CuttingChartColumn(R.string.arc_voltage_header, R.string.arc_voltage_column_name));
+        columns.set(5, new CuttingChartColumn(R.string.arc_height_header, R.string.arc_height_column_name));
+        columns.set(6, new CuttingChartColumn(R.string.pierce_height_header, R.string.pierce_height_column_name));
+        columns.set(7, new CuttingChartColumn(R.string.pierce_time_header, R.string.pierce_time_column_name));
+        columns.set(8, new CuttingChartColumn(R.string.kerf_offset_header, R.string.kerf_offset_column_name));
     }
 }
