8c863c792fb3de8eab2f881eabbb2e6dbe8e5285
Alexey Chernysh <alexey.chernysh@gmail.com>
2015-12-02 12:42:55 +0700

    3456

--- app/src/main/java/ru/chernysh/plasmatorchsetup/CuttingChart.java
+++ app/src/main/java/ru/chernysh/plasmatorchsetup/CuttingChart.java
@@ -6,6 +6,9 @@
 package ru.chernysh.plasmatorchsetup;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.graphics.Color;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -24,18 +27,18 @@ public class CuttingChart {
         context = cntxt;
         
         columns = new ArrayList<CuttingChartColumn>();
-        columns.add(new CuttingChartColumn(R.string.process_header, R.string.process_table));
-        columns.add(new CuttingChartColumn(R.string.current_header, R.string.current_column_name));
-        columns.add(new CuttingChartColumn(R.string.thickness_header, R.string.thickness_column_name));
-        columns.add(new CuttingChartColumn(R.string.purpose_header, R.string.purpose_table));
-        columns.add(new CuttingChartColumn(R.string.arc_voltage_header, R.string.arc_voltage_column_name));
-        columns.add(new CuttingChartColumn(R.string.arc_height_header, R.string.arc_height_column_name));
-        columns.add(new CuttingChartColumn(R.string.pierce_height_header, R.string.pierce_height_column_name));
-        columns.add(new CuttingChartColumn(R.string.pierce_time_header, R.string.pierce_time_column_name));
-        columns.add(new CuttingChartColumn(R.string.kerf_offset_header, R.string.kerf_offset_column_name));
+        columns.add(new CuttingChartColumn(R.string.process_header, R.string.process_table,true));
+        columns.add(new CuttingChartColumn(R.string.current_header, R.string.current_column_name,false));
+        columns.add(new CuttingChartColumn(R.string.thickness_header, R.string.thickness_column_name,false));
+        columns.add(new CuttingChartColumn(R.string.purpose_header, R.string.purpose_table,true));
+        columns.add(new CuttingChartColumn(R.string.arc_voltage_header, R.string.arc_voltage_column_name,false));
+        columns.add(new CuttingChartColumn(R.string.arc_height_header, R.string.arc_height_column_name,false));
+        columns.add(new CuttingChartColumn(R.string.pierce_height_header, R.string.pierce_height_column_name,false));
+        columns.add(new CuttingChartColumn(R.string.pierce_time_header, R.string.pierce_time_column_name,false));
+        columns.add(new CuttingChartColumn(R.string.kerf_offset_header, R.string.kerf_offset_column_name,false));
     }
     
-    public void fillHeader(){
+    public void fillHeaderView(){
         //clear obsolete content
         table.removeAllViews();
         // create header row
@@ -54,5 +57,121 @@ public class CuttingChart {
         // add row to layout
         headerRow.setBackgroundColor(Color.parseColor("#FFFFFFDD"));
         table.addView(headerRow);
+    }
+
+    public void fillContentView() {
+        //get row number
+        int rowNumber = columns.get(0).getDataLength();
+        //create & fill data rows
+        for(int i=0; i<rowNumber; i++){
+            TableRow dataRow = new TableRow(context);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+            dataRow.setLayoutParams(lp);
+
+            // fill header row
+            for (CuttingChartColumn column:columns) {
+                TextView dataCell = new TextView(context);
+                dataCell.setText(column.getData(i));
+                int columnNum = columns.indexOf(column);
+                dataRow.addView(dataCell, columnNum);
+            }
+
+            // add row to layout
+            dataRow.setBackgroundColor(Color.parseColor("#FFFFFFDD"));
+            table.addView(dataRow);
+        }
+    }
+
+    public void fillData() {
+        final String pref = App.getResourceString(R.string.preference_);
+        final String model_table_name = App.getResourceString(R.string.model_table);
+        final String material_table_name = App.getResourceString(R.string.material_table);
+
+        int modelKey = (new StoredKey(pref + model_table_name)).get();
+        int materialKey = (new StoredKey(pref + material_table_name)).get();
+
+        int[] processKey = getProcessList(modelKey);
+        if(processKey != null){
+            for (int aProcessKey : processKey)
+                fillTableForProcess(materialKey, aProcessKey);
+        }
+    }
+
+    private int[] getProcessList(int inverter) {
+
+        SQLiteDatabase db = (new DataBaseHelper()).getWritableDatabase();
+        String tableName = App.getResourceString(R.string.process_usage_table);
+        String filter = DataBaseHelper.getFilterEqualTo(R.string.model_table, inverter);
+        String process_column_header = App.getResourceString(R.string.process_table);
+        String[] columnList = {process_column_header};
+        Cursor cursor = db.query(true, tableName, columnList, filter, null, null, null, process_column_header, null, null);
+        int nOfRows = cursor.getCount();
+        int[] result = null;
+        if(nOfRows > 0){
+            result = new int[nOfRows];
+            int processKeyIndex = cursor.getColumnIndex(process_column_header);
+            if (cursor.moveToFirst()) {
+                for(int i=0; i<nOfRows; i++){
+                    result[i] = cursor.getInt(processKeyIndex);
+//                    Log.d(LOG_TAG, "getProcessList : process key[" + i + "]= " + result[i]);
+                    cursor.moveToNext();
+                }
+            }
+        }
+        cursor.close();
+        db.close();
+        return result;
+    }
+
+    private void fillTableForProcess(int materialKey, int processKey) {
+        if(materialKey <= 0) return;
+        String materialFilter = DataBaseHelper.getFilterEqualTo(R.string.material_table, materialKey);
+        if(processKey <= 0) return;
+        String processFilter = DataBaseHelper.getFilterEqualTo(R.string.process_table, processKey);
+        // find all "purpose" cases
+        SQLiteDatabase db = (new DataBaseHelper()).getWritableDatabase();
+        String processName = DataBaseHelper.getNameByKey(db, App.getResourceString(R.string.process_table), processKey);
+        String tableName = App.getResourceString(R.string.settings_table);
+        String filter = processFilter + " AND " + materialFilter;
+//        Log.d(LOG_TAG, "fillTableForProcess : filter 1: " + filter);
+        String purpose_column_name = App.getResourceString(R.string.purpose_table);
+        String[] columnList = {purpose_column_name};
+        Cursor cursor = db.query(true, tableName, columnList, filter, null, null, null, purpose_column_name, null, null);
+        int nOfPurpose = cursor.getCount();
+        if(nOfPurpose > 0){
+            int purposeKeyIndex = cursor.getColumnIndex(purpose_column_name);
+            if (cursor.moveToFirst()) {
+                for(int i=0; i<nOfPurpose; i++){
+                    int purposeKey = cursor.getInt(purposeKeyIndex);
+                    String purposeName = DataBaseHelper.getNameByKey(db, purpose_column_name, purposeKey);
+                    String filterWithPurpose = filter + " AND " + DataBaseHelper.getFilterEqualTo(R.string.purpose_table, purposeKey);
+//                    Log.d(LOG_TAG, "fillTableForProcess : filter 2: " + filterWithPurpose);
+                    Cursor purposeCursor = db.query(tableName, null, filterWithPurpose, null, null, null, null);
+                    int nOfSettings = columns.get(0).getDataLength();
+                    int nOfNewSettings = purposeCursor.getCount();
+                    if(nOfNewSettings>0){
+                        if(purposeCursor.moveToFirst()){
+                            for(CuttingChartColumn column:columns)
+                                column.updateIndex(purposeCursor);
+                            for(int j=0; j<nOfNewSettings; j++){
+                                // fill table data
+                                for(CuttingChartColumn column:columns)
+                                    if(column.isExternalKey()){
+                                        int id = purposeCursor.getInt(column.getIndex());
+                                        String name = DataBaseHelper.getNameByKey(db, column.getTableName(), id);
+                                        column.setData(nOfSettings+j,name);
+                                    } else {
+                                        double data = purposeCursor.getDouble(column.getIndex());
+                                        column.setData(nOfSettings+j,data);
+                                    }
+                                purposeCursor.moveToNext();
+                            }
+                        }
+                    }
+                    cursor.moveToNext();
+                }
+            }
+        }
+        cursor.close();
     }
 }
--- app/src/main/java/ru/chernysh/plasmatorchsetup/CuttingChartColumn.java
+++ app/src/main/java/ru/chernysh/plasmatorchsetup/CuttingChartColumn.java
@@ -5,32 +5,63 @@
 
 package ru.chernysh.plasmatorchsetup;
 
+import android.database.Cursor;
+
 import java.util.ArrayList;
 
 public class CuttingChartColumn {
 
     private final String header;
-    private final String dbAccessName;
-    ArrayList<Double> data;
 
-    public CuttingChartColumn(int headerStringId, int columnHeaderId){
+    public String getTableName() {
+        return dbAccessName;
+    }
+
+    private final String dbAccessName;
+    private ArrayList<String> data;
+    private int queryIndex = -1;
+    private final boolean externalKey;
+
+    public CuttingChartColumn(int headerStringId, int columnHeaderId, boolean exKey){
         header = App.getResourceString(headerStringId);
         dbAccessName = App.getResourceString(columnHeaderId);
+        externalKey = exKey;
+        data = new ArrayList<>();
     }
 
     public void setData(int recordNum, double value){
-        data.add(value);
+        Double tmp = value;
+        data.ensureCapacity(recordNum + 1);
+        data.add(recordNum, tmp.toString());
+    }
+
+    public void setData(int recordNum, String s){
+        data.ensureCapacity(recordNum + 1);
+        data.add(recordNum, s);
     }
 
     public String getHeader() {
         return header;
     }
 
-    public double getData(int recordNum){
-        return data.get(recordNum);
+    public int getDataLength() {
+        return data.size();
     }
 
-    public String getDbAccessName() {
-        return dbAccessName;
+    public String getData(int i) {
+        return data.get(i);
     }
+
+    public void updateIndex(Cursor cursor) {
+        queryIndex = cursor.getColumnIndex(dbAccessName);
+    }
+
+    public int getIndex() {
+        return queryIndex;
+    }
+
+    public boolean isExternalKey() {
+        return externalKey;
+    }
+
 }
